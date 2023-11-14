863c1358cb07374b4afb0577d9f841ffa40bc6b3
LudmiGonzalez <150303626+LudmiGonzalez@users.noreply.github.com>
2023-11-14 20:34:05 -0300

    Add files via upload

---
+++ dniCero.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: PC
+ * Fecha: 14/11/2023
+ * Hora: 20:09
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace proyecto_final
+{
+	/// <summary>
+	/// Description of dniCero.
+	/// </summary>
+	public class dniCero:Exception
+	{
+		public dniCero()
+		{
+		}
+	}
+}
